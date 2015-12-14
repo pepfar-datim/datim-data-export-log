fe0eaee475c2bd1b9e404c189a9908cf5774331b
Mark Polak <mark@thedutchies.com>
2015-12-14 15:27:18 +0100

    Add Prod webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -26,6 +26,23 @@ module.exports = {
         ]
     },
     plugins: [
+        // Replace any occurance of process.env.NODE_ENV with the string 'production'
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '\'production\'',
+            },
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: true,
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
